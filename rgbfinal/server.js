6fe0c274aaedc438e19fceea8cb17371b145fc92
Lisa-HuangZijin <zh2422@nyu.edu>
2026-05-04 02:51:36 +0800

    port

--- rgbfinal/server.js
+++ rgbfinal/server.js
@@ -4,7 +4,7 @@ const https = require("https");
 const fs = require("fs");
 
 const app = express();
-const portHTTPS = 4025;
+const portHTTPS = 4250;
 
 const options = {
   key: fs.readFileSync("keys-for-local-https/localhost-key.pem"),
